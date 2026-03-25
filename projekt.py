101c60e27d4ce6e6db3f7bb3df810d818f0b8ea9
kovaripeter <kovari.peter.2024i@bankitatabanya.hu>
2026-03-25 09:50:08 +0100

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -5,3 +5,33 @@ class Termek:
         self.keszletmennyiseg = keszletmennyiseg
     def adatok(self):
         print(f"A termék neve:{self.nev}, ára: {self.ar}, készleten:{self.keszletmennyiseg} db.")
+
+termek1 = Termek("Tej", 300, 30)
+termek2 = Termek("Kenyér", 600,21)
+termek3 = Termek("Alma",60,80)
+keszlet = [termek1,termek2,termek3]
+def adatokmentese(self):
+        f = open("keszlet.txt", "w", encoding="utf-8")
+        for termek in self.termekek:
+            f.write(f"{termek['nev']},{termek['ar']},{termek['db']}\n")
+            f.close()
+def uj_termekfelvetel(self):
+    print("\n--- Új termék felvétele ---")
+    
+    nev = input("Termék neve: ")
+    
+    try:
+        ar = int(input("Ár (Ft): "))
+        mennyiseg = int(input("Mennyiség (db): "))
+        uj_elem = {
+            "Termék": nev,
+            "Ár": ar,
+            "Mennyiség": mennyiseg
+        }
+        self.termekek.append(uj_elem)
+        print(f"Sikeresen hozzáadva: {nev}")
+        
+        self.adatokmentese()
+
+    except ValueError:
+        print("Hiba: Az ár és a mennyiség csak szám lehet!")
