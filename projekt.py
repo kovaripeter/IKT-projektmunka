4c4556eb4dc5b89a4864e28607f872bb04eab8b1
Levi1300 <osku.levente.balamber.2024i@bankitatabanya.hu>
2026-03-25 09:51:21 +0100

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -6,6 +6,7 @@ class Termek:
     def adatok(self):
         print(f"A termék neve:{self.nev}, ára: {self.ar}, készleten:{self.keszletmennyiseg} db.")
 
+print("--- RAKTÁRKEZELŐ RENDSZER v1.0 ---\n[INFO] Adatok betöltése a 'raktar.txt' fájlból... OK.\n1. Készlet listázása\n2. Eladás (Készlet csökkentése)\n3. Új termék felvétele\n4. Termék keresése\n5. Mentés és Kilépés\n")
 bemenet = int(input("Válasszon műveletet (1-5): "))
 
 while bemenet != 5:
