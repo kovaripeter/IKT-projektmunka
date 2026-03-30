fee01984961e4dcb74f433b77dd863802dde5fa3
Levi1300 <osku.levente.balamber.2024i@bankitatabanya.hu>
2026-03-30 10:22:57 +0200

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -1,4 +1,17 @@
-def eladas():
+def betoltes(fajl):
+    betolt = termekek
+    if betolt == True:
+        termekek = open("raktar.txt", "w", encoding="utf-8")
+        for sor in f:
+            adatok = sor.strip().split(",")
+            if len(adatok) == 3:
+                nev, ar, mennyiseg = adatok
+                termekek.append(Termek(nev, int(ar), int(mennyiseg)))
+    else:
+        termekek = []
+
+
+def eladas(termekek):
     print("--- ELADÁS ---")
     nev = input("Melyik terméket vásárolták meg? ")
 
@@ -30,24 +43,29 @@ def kereses(termekek):
             print(f"[TALÁLAT] {t.nev} - Ár: {t.ar} Ft, Készlet: {t.mennyiseg} db")
             talalat = True
 
+
+
+
+fut = True
+while fut:
+    print("--- RAKTÁRKEZELŐ RENDSZER v1.0 ---\n[INFO] Adatok betöltése a 'raktar.txt' fájlból... OK.\n1. Készlet listázása\n2. Eladás (Készlet csökkentése)\n3. Új termék felvétele\n4. Termék keresése\n5. Mentés és Kilépés\n")
+
     bemenet = int(input("Válasszon műveletet (1-5): "))
 
-    fut = True
-    while fut:
-        if bemenet == 1:
-            listazas(termekek)
-        elif bemenet == 2:
-            uj_termek(termekek)
-        elif bemenet == 3:
-            eladas(termekek)
-        elif bemenet == 4:
-            kereses(termekek)
-        elif bemenet == 5:
-            fut = False  
-            print("[INFO] Adatok mentése a 'raktar.txt' fájlba... KÉSZ.Viszontlátásra!")
-            break
-        else:
-            print("[HIBA] Hibás választás!")
+    if bemenet == 1:
+        listazas(termekek)
+    elif bemenet == 2:
+        uj_termek(termekek)
+    elif bemenet == 3:
+        eladas(termekek)
+    elif bemenet == 4:
+        kereses(termekek)
+    elif bemenet == 5:
+        fut = False  
+        print("[INFO] Adatok mentése a 'raktar.txt' fájlba... KÉSZ.Viszontlátásra!")
+        break
+    else:
+        print("[HIBA] Hibás választás!")
 
 
 
