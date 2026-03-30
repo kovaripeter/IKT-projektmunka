9e42ab3a1fa60f18fdcc4c56952f0b54731a32ff
Levi1300 <osku.levente.balamber.2024i@bankitatabanya.hu>
2026-03-30 08:31:17 +0200

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -10,16 +10,16 @@ termek1 = Termek("Tej", 300, 30)
 termek2 = Termek("Kenyér", 600,21)
 termek3 = Termek("Alma",60,80)
 keszlet = [termek1,termek2,termek3]
+
 def adatokmentese(self):
         f = open("keszlet.txt", "w", encoding="utf-8")
         for termek in self.termekek:
             f.write(f"{termek['nev']},{termek['ar']},{termek['db']}\n")
             f.close()
+
 def uj_termekfelvetel(self):
     print("\n--- Új termék felvétele ---")
-    
     nev = input("Termék neve: ")
-    
     try:
         ar = int(input("Ár (Ft): "))
         mennyiseg = int(input("Mennyiség (db): "))
@@ -35,7 +35,33 @@ def uj_termekfelvetel(self):
 
     except ValueError:
         print("Hiba: Az ár és a mennyiség csak szám lehet!")
-print("--- RAKTÁRKEZELŐ RENDSZER v1.0 ---\n[INFO] Adatok betöltése a 'raktar.txt' fájlból... OK.\n1. Készlet listázása\n2. Eladás (Készlet csökkentése)\n3. Új termék felvétele\n4. Termék keresése\n5. Mentés és Kilépés\n")
+
+def eladas(termekek):
+    print("\n--- ELADÁS ---")
+    nev = input("Melyik terméket vásárolták meg? ")
+    for t in termekek():
+        if t.nev.lower() == nev.lower():
+            try:
+                db = int(input("Hány darabot? "))
+                if db <= 0:
+                    raise ValueError
+            except:
+                print("[HIBA] Hibás mennyiség!")
+                return
+
+            if db > t.mennyiseg:
+                print("[HIBA] Nincs elég készlet!\n")
+                return
+
+            t.mennyiseg -= db
+            osszeg = db * t.ar
+
+            print(f"[SIKER] Eladva: {db} db {t.nev}. Fizetendő: {osszeg} Ft.")
+            print(f"(Új készlet: {t.mennyiseg} db)\n")
+            return
+
+    print("[HIBA] Nincs ilyen termék!\n")
+
 bemenet = int(input("Válasszon műveletet (1-5): "))
 
 while bemenet != 5:
@@ -48,4 +74,3 @@ while bemenet != 5:
     elif bemenet == 4:
         pass
     bemenet = int(input("Válasszon műveletet (1-5): "))
-    
